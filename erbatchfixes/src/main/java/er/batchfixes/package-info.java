fcdfe46b79846373a9f110496578e277859d0dd5
G Brown <medsoftw@gmail.com>
2012-11-11 17:13:29 -0500

    doc updates

--- erbatchfixes/src/main/java/er/batchfixes/package-info.java
+++ erbatchfixes/src/main/java/er/batchfixes/package-info.java
@@ -1,10 +1,13 @@
 /**
-This ERBatchFixes framework allows you to have your application do batch jobs at startup time. These jobs
+This ERBatchFixes framework allows you to have your application do batch jobs at startup time. 
+Often after database migrations there may be needed modifications to the data in the database. This package
+allows you to more easily create these changes through a batch job, run after a migration, but before your application
+starts up.  These jobs
 are remembered in a _ERBatchFixes db table. Also, a log4j logger is setup so you can log during the batch job, and
 this log is stored with the batch job info, as is start time, end time. To use this,
-create a subclass of ERBatchFixesParent, which will then do whatever batch jobs/processing you want. 
+create a subclass of ERBatchFixesParent, which you will then use to do whatever batch jobs/processing you want. 
 The ERBatchFixesParent handles storing fix event information for you; things like: has the fix been applied, when? the error log, etc.
-ERMigrations is used to create the db table.
+ERMigrations is used to create the db table _ERBatchFixes.
 <p/>
 To use this do 3 things:
 <p/>
@@ -37,6 +40,12 @@ The fixes will be remembered (along with an error log) in a chosen database. Set
 your Properties to point to the DB where you want the fix events persisted.
 
 <pre>
+# these migrations happen after main program initialization but before batch fixes..
+er.migration.migrateAtStartup=true
+er.migration.createTablesIfNecessary=true
+er.migration.modelNames=ERBatchFixes,myOtherModels,etc
+ERBatchFixes.MigrationClassPrefix=er.batchfixes.migrations.ERBatchFixes
+
 # property must be true to use this
 er.batchfixes.dofixes = true
 ERBatchFixes.URL = jdbc:FrontBase://localhost/MyDBProduction
@@ -46,12 +55,6 @@ ERBatchFixes.DBDriver =
 ERBatchFixes.DBPlugin =
 ERBatchFixes.DBJDBCInfo =
 
-# these migrations happen after main program initialization..
-er.migration.migrateAtStartup=true
-er.migration.createTablesIfNecessary=true
-er.migration.modelNames=ERBatchFixes,myOtherModels,etc
-ERBatchFixes.MigrationClassPrefix=er.batchfixes.migrations.ERBatchFixes
-
 
 </pre>
 This framework will create a _ERBatchFixes table, via migrations to store fix event information.
@@ -60,8 +63,5 @@ with all instances. As ERXJDBCMigrationLock says: "if true, the tables and model
 * *ONLY SET THIS IF YOU ARE RUNNING IN DEVELOPMENT MODE OR WITH A SINGLE INSTANCE*. 
 *If you are running multiple instances, the instances will not be able to acquire locks properly
 * and you may end up with multiple instances attempting to create lock tables and/or failing to startup properly.
-
-
-
  */
 package er.batchfixes;
